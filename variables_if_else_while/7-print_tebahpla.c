d9b13394c12cd15106a0d27d128293c806d565d0
charlybarrio <5757@holbertonstudents.com>
2022-09-28 10:42:59 -0700

    Task7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - task5
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int xyy;
+
+	for (xyy = 'z'; xyy >= 'a'; xyy--)
+		putchar (xyy);
+	putchar ('\n');
+	return (0);
+}
