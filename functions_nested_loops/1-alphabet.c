d48e8ad7c3b4aedb9962356206a0770b510bce0e
charlybarrio <5757@holbertonstudents.com>
2022-09-29 07:19:51 -0700

    Task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Task1
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	int az;
+
+	for (az = 'a'; az <= 'z'; az++)
+		_putchar (az);
+	_putchar ('\n');
+}
