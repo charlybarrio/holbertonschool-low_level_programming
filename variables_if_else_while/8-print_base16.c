17f10f5277fa97f1a74bd8f9a1a43bc9f2faf534
charlybarrio <5757@holbertonstudents.com>
2022-09-29 05:32:35 -0700

    Task8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Task8
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	char x;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar (n);
+	for (x = 'a'; x <= 'f'; x++)
+		putchar (x);
+	putchar ('\n');
+	return (0);
+}
