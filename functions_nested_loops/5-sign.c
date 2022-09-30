58ba944f3cfc5a33b32762d5583346c9de200953
charlybarrio <5757@holbertonstudents.com>
2022-09-30 05:54:47 -0700

    task5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,18 +8,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar ('+');
-		_putchar (',');
-		_putchar (' ');
 		_putchar ('1');
+		_putchar ('+');
 		_putchar ('\n');
 	}
 
 	else if (n == 0)
 	{
 		_putchar ('0');
-		_putchar (',');
-		 _putchar (' ');
 		 _putchar ('0');
 		 _putchar ('\n');
 	}
@@ -27,10 +23,8 @@ int print_sign(int n)
 	else
 	{
 		_putchar ('-');
-		_putchar (',');
-		_putchar (' ');
-		_putchar ('-');
 		_putchar ('1');
+		_putchar ('-');
 		_putchar ('\n');
 	}
 	return (0);
