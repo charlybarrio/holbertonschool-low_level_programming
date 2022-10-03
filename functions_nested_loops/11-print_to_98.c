29049f91bd9a4b5f9d9622b5fee09dd6eb8f9e66
charlybarrio <5757@holbertonstudents.com>
2022-10-03 07:06:51 -0700

    Task11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *print_to_98 - Task11
+ *@n: int
+ *Return: always 0
+ */
+void print_to_98(int n)
+
+{
+	if (n < 98)
+	{
+		for (; n <= 97; n++)
+		{
+			printf("%d, ", n);
+		}
+		{
+		printf("98\n");
+		}
+	}
+	else if (n == 98)
+	{
+		printf("98\n");
+	}
+	else
+	{
+		for (; n >= 99; n--)
+		{
+			printf("%d, ", n);
+		}
+		{
+			printf("98\n");
+		}
+	}
+}
