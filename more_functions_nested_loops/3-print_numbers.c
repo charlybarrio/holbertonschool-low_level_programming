fa3c2026a15ed2213c5b14d15dad9344930df47a
charlybarrio <5757@holbertonstudents.com>
2022-10-04 06:44:46 -0700

    Task3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - Task3
+ *
+ *Return: always 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar (a);
+	}
+	_putchar ('\n');
+}
