4bd40a22527862bfb37d6cb89c5b7202f3471ba0
charlybarrio <5757@holbertonstudents.com>
2022-10-05 08:32:02 -0700

    Task9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - Task9
+ *
+ * Returne: always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 99; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz ");
+		else if (a % 3 == 0)
+			printf("Fizz ");
+		else if (a % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", a);
+	}
+	printf("Buzz");
+	printf("\n");
+	return (0);
+}
