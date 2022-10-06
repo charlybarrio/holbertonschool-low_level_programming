c01319e703bb856d054b39a30444c3d7851028f7
charlybarrio <5757@holbertonstudents.com>
2022-10-06 09:15:29 -0700

    Task4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 /**
- * print_rev
+ * print_rev - Task4
  * @s: char
  * Return: always 0
  */
@@ -13,9 +13,9 @@ void print_rev(char *s)
 	l = strlen(s);
 	for (a = l - 1; a >= 0; a--)
 	{
-		printf("%c", s[a]);
+		_putchar ('\n');
 	}
 	{
-		_putchar ('\n');
+		printf("%c", s[a]);
 	}
 }
