04cd060aa6193bd3fc8d4a7f1c318d47dc5086a3
charlybarrio <5757@holbertonstudents.com>
2022-10-06 09:01:15 -0700

    task4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev
+ * @s: char
+ * Return: always 0
+ */
+void print_rev(char *s)
+{
+	int l, a;
+
+	scanf("%c", s);
+	l= strlen(s);
+	for (a = l -1; a >= 0; a--)
+	{
+		printf("%c\n", s[a]);
+	}
+}
