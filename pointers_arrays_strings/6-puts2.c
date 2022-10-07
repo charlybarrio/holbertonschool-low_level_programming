b15f2b1d2ce4251556166069992bcb9e6cd1aab8
charlybarrio <5757@holbertonstudents.com>
2022-10-07 08:25:44 -0700

    Task6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - task6
+ *@str: char
+ * Retunr: always 0
+ */
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; a != '\0'; a++)
+	if (a % 2 == 0)
+		printf("%c\n", str[a]);
+}
