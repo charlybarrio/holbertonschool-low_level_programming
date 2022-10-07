1ac9473a89610f22f6fc8842749987629c638117
charlybarrio <5757@holbertonstudents.com>
2022-10-07 08:29:54 -0700

    task6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,12 @@ void puts2(char *str)
 {
 	int a;
 
-	for (a = 0; a != '\0'; a++)
-	if (a % 2 == 0)
-		printf("%c\n", str[a]);
+	for (a = 0; str[a] != '\0'; a++)
+		if (a % 2 == 0)
+		{
+			printf("%c", str[a]);
+		}
+	{
+	_putchar ('\n');
+	}
 }
