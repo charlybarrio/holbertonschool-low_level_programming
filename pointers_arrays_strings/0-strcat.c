95481694f8f1d27cbe63c660a67ad2463b016268
charlybarrio <5757@holbertonstudents.com>
2022-10-11 09:02:22 -0700

    Task0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,26 @@
-s
+#include "main.h"
+/**
+ *_strcat
+ *@dest: char
+ *@src: char
+ *Return: always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		++a;
+	}
+	for (b = 0; src[b] != '\0'; b++, a++)
+	{
+		dest[a] = src[b];
+	}
+	{
+		dest[a] = '\0';
+	}
+	return (dest);
+}
