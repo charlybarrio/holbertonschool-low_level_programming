cabdbc01ada8885af57c97521018e35bb2af75bd
charlybarrio <5757@holbertonstudents.com>
2022-10-11 07:58:55 -0700

    task9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _strcpy
- *dest: char
- *src: char
+ *_strcpy - Task9
+ *@dest: char
+ *@src: char
  * Return: always 0
  */
 char *_strcpy(char *dest, char *src)
 {
 	int a;
 
-	for(a = 0; src[a] != '\0'; a++)
+	for (a = 0; src[a] != '\0'; a++)
 		dest[a] = src[a];
 	return (dest);
 }
