489e86b96d0e7fecfe11cb867b0e675272747473
charlybarrio <5757@holbertonstudents.com>
2022-10-12 07:09:29 -0700

    task2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,12 +8,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a;
 	int b;
 
-	for (a = 0; dest[a] != '\0'; a++)
-		for (b = 0; b <= n; b++)
-		dest[a] = src[b];
-
-	return (0);
+		for (b = 0; b < n; b++)
+		{
+			dest[0] = src[b];
+		}
+	return (dest);
 }	 
