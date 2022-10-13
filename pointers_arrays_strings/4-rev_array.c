d66e1e8ce7d36db1172f2f87c2c2ffb2d513e2d2
charlybarrio <5757@holbertonstudents.com>
2022-10-13 07:51:30 -0700

    Task4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - task4
+ *@a: int
+ *@n: int
+ * Return: always 0
+ */
+void reverse_array(int *a, int n)
+{
+	int b, c;
+
+	for (b = 0; b < n / 2; b++)
+	{
+		c = a[b];
+		a[b] = a[n - b - 1];
+		a[n - b - 1] = c;
+	}
+}
