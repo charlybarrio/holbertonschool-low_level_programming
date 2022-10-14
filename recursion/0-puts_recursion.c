20703a3b602178a18d418ca8a289f77ab3404774
charlybarrio <5757@holbertonstudents.com>
2022-10-14 07:55:13 -0700

    task0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_puts_recursion - Task0
+ *@s: char
+ *Return: always 0
+ */
+void _puts_recursion(char *s)
+{
+	if(*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		if(*s == '\0')
+			_putchar ('\n');
+	_puts_recursion(s);
+	}
+}
