2a6ed5577251dd9bd15f1a202a8b5cf025ba1a89
charlybarrio <5757@holbertonstudents.com>
2022-10-17 06:14:08 -0700

    task1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -4,12 +4,12 @@
  *@s: char
  *Return: always 0
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 	else
 		_putchar ('\n');
