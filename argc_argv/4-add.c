a60d71db7a7da13bcf43d275fefef80985f262e6
charlybarrio <5757@holbertonstudents.com>
2022-10-18 18:02:11 -0700

    task4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *main - task2
+ *@argv: char
+ *@argc: int
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int t = 0;
+
+	if (argc < 1)
+		printf("0\n");
+	return (0);
+
+	for (a = 1; a < argc; a++)
+	{
+		if (!atoi(argv[a]))
+		{
+			printf("Error\n");
+				return (1);
+		}
+		t += atoi(argv[a]);
+	}
+	printf("%d\n", t);
+	return (0);
+}
