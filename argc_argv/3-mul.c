6ae82745d04a1ae3038573a1a60153c8b448f815
charlybarrio <5757@holbertonstudents.com>
2022-10-18 17:24:03 -0700

    task3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *main - task2
+ *@argv: char
+ *@argc: int
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+	int t;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+	return (1);
+	}
+	
+	else 
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		t = x * y;
+
+			printf("%d\n", t);
+		return (0);
+	}
+}
