704aaa8705a60ab7785fbb5a4b900d7e3306331f
charlybarrio <5757@holbertonstudents.com>
2022-10-19 07:20:03 -0700

    task0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ **create_array - funtion that creat an array
+ *@size: int
+ *@c: char
+ *Return: always 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int n;
+	char *d ;
+
+	d = malloc(sizeof(char) * size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else if (d != NULL)
+	{
+		for (n = 0; n < size; n++)
+		{
+			d[n] = c;
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (d);
+}
