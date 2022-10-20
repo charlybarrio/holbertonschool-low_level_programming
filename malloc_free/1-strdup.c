fbf58e9a5d549532368202858c7d2cfc3adbe70d
charlybarrio <5757@holbertonstudents.com>
2022-10-20 16:12:27 -0700

    task1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,13 +9,13 @@ char *_strdup(char *str)
 {
 	char *s;
 	unsigned int a;
-	
+
 	if (str == NULL)
 		return (NULL);
-	s = malloc(sizeof(char) * strlen(str));
+	s = malloc(sizeof(char) * strlen(str) + 1);
 	if (s == NULL)
 		return (NULL);
-	for (a = 0; a <= strlen(str); a++)
+	for (a = 0; a <= strlen(str) + 1; a++)
 		s[a] = str[a];
 	return (s);
 }
