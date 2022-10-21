d2de66f9358a906dda7dc2fa6f9b924c71611a55
charlybarrio <5757@holbertonstudents.com>
2022-10-20 17:57:50 -0700

    task2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,38 +7,36 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s3, *s4, *s5;
-	unsigned int a, b;
+	char *s3;
+	char *s4;
+	unsigned int a;
+	unsigned int b;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	else if (s2 == NULL)
-	{
-		s1 = "";
-	}
-	s3 = malloc(sizeof(char) * strlen(s1));
+	s3 = malloc(sizeof(char) * strlen(s1) + 1);
 	s4 = malloc(sizeof(char) * strlen(s2));
 	if (s3 == NULL)
-        {
-                return (NULL);
-        }
-        else if (s4 == NULL)
-        {
-                return (NULL);
-        } 
-	for (a = 0; a <= strlen(s1); a++)
 	{
-	s3[a] = s1[a];
+		s3 = "";
 	}
-	for (b = 0; b <= strlen(s2); b++)
+	else if (s4 == NULL)
 	{
-	s4[b] = s2[b];
+		s4 = "";
 	}
-	s5 = malloc(sizeof(char) * strlen(s1) - 4);
-	if (s5 == NULL)
+	else if (s3 == NULL && s4 == NULL)
+	{
 		return (NULL);
-	s5 = strcat(s3, s4);
-	return (s5);
+	}
+	if (s3 != NULL && s4 != NULL)
+	{
+		for (a = 0; a <= strlen(s1) + 1; a++)
+		{
+			s3[a] = s1[a];
+		}
+		for (b = 0; b <= strlen(s2) ; b++)
+		{
+			s4[b] = s2[b];
+		}
+		s3 = strcat(s3, s4);
+	}
+	return (s3);
 }
