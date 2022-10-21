a803b242adeaf01385bb06d7ee90773632f24cb4
charlybarrio <5757@holbertonstudents.com>
2022-10-20 17:46:25 -0700

    task2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,26 +12,14 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
 	}
 	else if (s2 == NULL)
 	{
-		return ("");
+		s1 = "";
 	}
-	else if (s1 == NULL && s2 == NULL)
-		return ("");
 	s3 = malloc(sizeof(char) * strlen(s1));
 	s4 = malloc(sizeof(char) * strlen(s2));
-	if (s3 == NULL)
-        {
-                return ("");
-        }
-        else if (s4 == NULL)
-        {
-                return ("");
-        }
-        else if (s3 == NULL && s4 == NULL)
-                return ("");
 	for (a = 0; a <= strlen(s1); a++)
 	{
 	s3[a] = s1[a];
