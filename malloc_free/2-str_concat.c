26cf95cc1f870d82302e280a02027a46dd9e945a
charlybarrio <5757@holbertonstudents.com>
2022-10-20 17:23:24 -0700

    task2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,30 +7,36 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int a;
-	int b = 0;
+	char *s3, *s4, *s5;
+	unsigned int a, b;
 
 	if (s1 == NULL)
 	{
 		return (s2);
 	}
-	if (s2 == NULL)
+	else if (s2 == NULL)
 	{
 		return (s1);
 	}
-	s3 = malloc(sizeof(char) * (strlen(s1) + strlen(s2)) + 1);
-	if (s3 == NULL)
-	{
-		return (NULL);
-	}
-
-	for (a = 0; a <= strlen(s1); a++)
-	{
-		s3[a] = s1[a];
-		b++;
-	}
-	for (a = 0; a <= strlen(s2); a++)
-		s3[b++] = s2[a];
-	return (s3);
-}	
+	s3 = malloc(sizeof(char) * strlen(s1));
+        s4 = malloc(sizeof(char) * strlen(s2));
+        if (s3 == NULL)
+        {
+                return (NULL);
+        }
+        else if (s4 == NULL)
+        {
+                return (NULL);
+        }
+        for (a = 0; a <= strlen(s1) + 1; a++)
+        {
+                s3[a] = s1[a];
+        }
+        for (b = 0; b <= strlen(s2) + 1; b++)
+        {
+                s4[b] = s2[b];
+        }
+        s5 = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+	s5 = strcat(s3, s4);
+        return (s5);
+}
