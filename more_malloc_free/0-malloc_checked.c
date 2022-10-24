32c240d5942e09079befc4f8e61ab78b54892cca
charlybarrio <5757@holbertonstudents.com>
2022-10-24 15:57:53 -0700

    task0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+**malloc_checked - allocates memory
+*@b:int
+*Return: always 0
+*/
+void *malloc_checked(unsigned int b)
+{
+	unsigned int **a;
+	unsigned int *c;
+	c = malloc(sizeof(int) * b);
+	a = &c;
+
+	if (c == NULL)
+		return(NULL);
+	free(c);
+	return (a);
+}
