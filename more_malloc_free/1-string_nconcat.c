47ce229742cb686f09bf7ff2b93a60faf6c71e91
charlybarrio <5757@holbertonstudents.com>
2022-10-24 16:53:05 -0700

    task1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-**string_nconcat - concatenates two string
+* *string_nconcat - concatenates two string
 *@s1:string1
 *@s2:string2
 *@n:q of char
+*Return: always 0
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -20,11 +21,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	s3 = malloc(sizeof(char) * strlen(s1) + 1);
+	s3 = malloc(sizeof(char) * strlen(s1));
 	s4 = malloc(sizeof(char) * n);
 	if (s3 != NULL && s4 != NULL)
 	{
-		for (a = 0; a <= strlen(s1) + 1; a++)
+		for (a = 0; a <= strlen(s1); a++)
 			s3[a] = s1[a];
 		for (b = 0; b <= n; b++)
 			s4[b] = s2[b];
