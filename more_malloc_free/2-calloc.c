e8998d05fe8fc7d2f626c5f75ea532e1bea3976f
charlybarrio <5757@holbertonstudents.com>
2022-10-25 08:18:05 -0700

    task2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * *_memset - task0
+ * @s: char
+ * @b: char
+ * @n: int
+ * Return: always 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+	return (s);
+}
+/**
+**_calloc - allocates memory for an array, using malloc
+*@nmemb: int
+*@size: int
+*Return: always 0
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ns;
+
+	ns = malloc(size * nmemb);
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (ns == NULL)
+		return (NULL);
+	_memset(ns, 0, size * nmemb);
+		return(ns);
+}
