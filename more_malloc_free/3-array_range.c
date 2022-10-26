fe589db71a8436fb07989e0d7ee6f67b818b4be4
charlybarrio <5757@holbertonstudents.com>
2022-10-26 14:21:44 -0700

    task3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ **array_range - creates an array of integers
+ *@max: int
+ *@min: int
+ *Return: always 0
+ */
+int *array_range(int min, int max)
+{
+	int *oni;
+	int chan;
+
+	if (min > max)
+		return (NULL);
+
+	oni = malloc(sizeof(int) * (max - min + 1));
+
+	if (!oni)
+		return (NULL);
+	for (chan = min; chan <= max; chan++)
+		oni[chan] = min;
+	return (oni);
+}
