841cb7e1f2bf8116cfd3fa557822174c6ba2e038
charlybarrio <5757@holbertonstudents.com>
2022-10-27 06:02:42 -0700

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+#include "main.h"
+/**
+ *struct dog - Define a new type struct dog
+ *@name:char
+ *@age:float
+ *@owner:char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
