9cdcfcf92cdb95265a4270dfc9e70fa469035f44
charlybarrio <5757@holbertonstudents.com>
2022-10-31 16:02:36 -0700

    task2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -10,9 +10,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == 0)
-		return;
+		return (-1);
 	if (cmp == 0)
-		return;
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
