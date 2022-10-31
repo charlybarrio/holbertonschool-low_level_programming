5c0c6924161cfebbb01501acaa1e9850a328a52a
charlybarrio <5757@holbertonstudents.com>
2022-10-31 10:59:01 -0700

    task4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,1 +1,54 @@
-std
+#include "dog.h"
+#include <string.h>
+/**
+ **new_dog
+ *name:char
+ *@age:float
+ *@owner:char
+ *Return:always 0
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *perrito;
+	char *n2, *o2;
+
+	perrito = malloc(sizeof(dog_t));
+
+	if (perrito == NULL)
+		return (NULL);
+	n2 = _strdup(name);
+	if (n2 == NULL)
+	{
+		free(n2);
+		return (NULL);
+	}
+	o2 = _strdup(owner);
+	if (o2 == NULL)
+	{
+		free(o2);
+		return (NULL);
+	}
+	perrito->name = n2;
+	perrito->age = age;
+	perrito->owner = o2;
+return (perrito);
+}
+/**
+ *_strdup - Task1
+ *@str: char
+ *Return: always 0
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	unsigned int a;
+
+	if (str == NULL)
+		return (NULL);
+	s = malloc(sizeof(char) * strlen(str) + 1);
+	if (s == NULL)
+		return (NULL);
+	for (a = 0; a <= strlen(str) + 1; a++)
+		s[a] = str[a];
+	return (s);
+}
