146a6854cbfbef6e78502f2d32142d7a54448be7
charlybarrio <5757@holbertonstudents.com>
2022-10-31 15:47:12 -0700

    task1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- *array_iterator - executes a function given as a parameter on each element of an array
+ *array_iterator - function given as a parameter on each element
  *@array:int
  *@action:int
  *@size:size
