bac63a49aa23ba2c22681d6b7baa64c1751c4d2a
charlybarrio <5757@holbertonstudents.com>
2022-11-01 07:56:13 -0700

    task2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+/**
+ *print_strings
+ *@separator:char
+ *@n:int
+ *@...:parameter
+ *Return:always 0
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list q;
+	char *str;
+
+	va_start(q, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(q, char *);
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+			if (separator != NULL && i != n - 1)
+			{
+				printf("%s", separator);
+			}
+		}
+	}
+	printf("\n");
+}
