5bcffe19955c140d2465a3938da240386a7da3f5
charlybarrio <5757@holbertonstudents.com>
2022-11-19 06:01:05 -0800

    task0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *print_listint - print all elementsof a list
+ *@h:list
+ *Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
