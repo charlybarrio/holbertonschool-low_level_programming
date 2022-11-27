99a77b51a2fc4b739c2dce3172063e14a6e1cecc
charlybarrio <5757@holbertonstudents.com>
2022-11-27 15:36:23 -0800

    task7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -8,37 +8,45 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *cop = *h, *new;
-	unsigned int node;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->prev = NULL;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
+
+	new = NULL;
 	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
 	{
-		new->next = cop;
-		*h = new;
-		return (new);
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
-	for (node = 0; node < (idx - 1); node++)
-	{
-		if (cop == NULL || cop->next == NULL)
-			return (NULL);
 
-		cop = cop->next;
-	}
-	new->next = cop->next;
-	if (cop != NULL)
-	{
-		while (cop->prev != NULL)
-			cop = cop->prev;
-	}
-	cop->next = new;
-	if (cop != NULL)
-		cop->prev = new;
-	*h = new;
 	return (new);
 }
-
