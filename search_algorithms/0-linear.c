09c26cde1373f01d5ba4de3db74e0bab7bd6cc38
charlybarrio <5757@holbertonstudents.com>
2023-01-09 11:58:00 -0800

    header and lineal search

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,10 @@
+#include "search_algos"
+/**
+ * linear_search - Linear algorithm
+ * @*array: pointer to the first element of the array
+ * @size: the size of the array
+ * @value: the valude to search
+ */
+int linear_search(int *array, size_t size, int value)
+{
+
