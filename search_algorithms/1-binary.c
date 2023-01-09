092855342f7a5316936b61768e021838bf32ebc6
charlybarrio <5757@holbertonstudents.com>
2023-01-09 13:41:06 -0800

    binary algo

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -5,23 +5,24 @@
  *@array: pointer to the first element
  *@size: number of elements
  *@value: the value to search
- *@Return: the index where is value,-1 if it fails
+ *Return: the index where is value,-1 if it fails
  */
 int binary_search(int *array, size_t size, int value)
 {
 	int max = size - 1, min = 0;
-	int half = (min + max) / 2;
+	int half;
 	int a;
 
 	if (array == NULL)
 		return (-1);
 
-	if (min <= max)
+	while (min <= max)
 	{
+		half = (min + max) / 2;
 		printf("Searching in array: ");
 		for (a = min; a <= max; a++)
 		{
-			if (a != min)
+			if (a > min)
 				printf(", ");
 			printf("%d", array[a]);
 		}
@@ -30,12 +31,13 @@ int binary_search(int *array, size_t size, int value)
 		{
 			return (half);
 		}
+		else if (value > array[half])
+		{
+			min = half + 1;
+		}
 		else
 		{
-			if (value >= array[half])
-				min = half + 1;
-			else
-				max = half -1;
+			max = half - 1;
 		}
 	}
 	return (-1);
