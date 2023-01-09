fb2e37825bc43f1d5ccf6249e66d954648ddea59
charlybarrio <5757@holbertonstudents.com>
2023-01-09 12:02:14 -0800

    task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,10 +1,24 @@
-#include "search_algos"
+#include "search_algos.h"
 /**
  * linear_search - Linear algorithm
  * @*array: pointer to the first element of the array
  * @size: the size of the array
  * @value: the valude to search
+ * return: value or -1
  */
 int linear_search(int *array, size_t size, int value)
 {
+	unsigned int a = 0;
 
+	for (a = 0; a <= size -1; a++)
+	{
+		if (value == array[a])
+		{
+			printf("Value checked array[%d] = [%d]\n",a, array[a]);
+			return (array[a]);
+		}
+		else
+			printf("Value checked array[%d] = [%d]\n",a, array[a]);
+	}
+	return (-1);
+}
